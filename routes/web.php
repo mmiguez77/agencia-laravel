3478bdd469d6a2e6a1b3d242e4a4e6f0f44262c0
mmiguez77 <martinmiguez77@gmail.com>
2021-12-21 22:03:31 -0300

    Vista Destinos y Regiones

--- routes/web.php
+++ routes/web.php
@@ -75,3 +75,99 @@ Route::post('/agregarRegion', function () {
                 ->with(['mensaje' => 'Region: '.$regNombre.' agregada correctamente.']);
 
 });
+
+Route::get('/modificarRegion/{id}', function ($idRegion)
+{
+    //obtenemos datos de la región
+    /*$region = DB::select('
+                SELECT * FROM
+                    regiones
+                    WHERE idRegion = :id', [$idRegion] );*/
+    $region = DB::table('regiones')
+                    ->where('idRegion', $idRegion)
+                    ->first();  // fetch()
+
+    return view('modificarRegion',
+                [ 'region'=>$region ]
+            );
+});
+
+Route::post('/modificarRegion', function ()
+{
+    $regNombre = $_POST['regNombre'];
+    $idRegion = $_POST['idRegion'];
+    /*DB::update('UPDATE regiones
+                    SET regNombre = :regNombre
+                  WHERE idRegion  = :idRegion',
+                    [ $regNombre, $idRegion ]);*/
+    DB::table('regiones')
+            ->where( 'idRegion', $idRegion )
+            ->update( [ 'regNombre' => $regNombre ] );
+    //redirección con reporte ok
+    return redirect('/adminRegiones')
+        ->with(['mensaje'=>'Region: '.$regNombre.' agregada correctamente.']);
+});
+
+
+// DESTINOS
+
+Route::get('/adminDestinos', function () {
+
+    /*$destinos = DB::select('
+        SELECT
+        idDestino, destNombre, destPrecio,
+        regNombre
+        FROM destinos as d
+        JOIN regiones as r
+        ON d.idRegion = r.idRegion
+        ');*/
+    $destinos = DB::table('destinos as d')
+        ->select('idDestino', 'destNombre', 'destPrecio','regNombre')
+        ->join('regiones as r', 'd.idRegion', '=', 'r.idRegion' )
+        ->get(); 
+
+    return view('adminDestinos', [ 'destinos' => $destinos ]);
+});
+
+Route::get('/agregarDestino', function () {
+
+    //obtenemos listado de regiones
+    $regiones = DB::table('regiones')->get();
+
+    return view('agregarDestino', [ 'regiones' => $regiones ]);
+});
+
+Route::post('/agregarDestino', function () {
+    //capturamos datos enviados por el form
+    $destNombre = $_POST['destNombre'];
+    $idRegion = $_POST['idRegion'];
+    $destPrecio = $_POST['destPrecio'];
+    $destAsientos = $_POST['destAsientos'];
+    $destDisponibles = $_POST['destDisponibles']; 
+
+    // Insertamos en la DB
+    DB::table('destinos')
+        ->insert(
+            [
+                'destNombre'=>$destNombre,
+                'idRegion'=>$idRegion, 
+                'destPrecio'=>$destPrecio,
+                'destAsientos'=>$destAsientos,
+                'destDisponibles'=>$destDisponibles
+            ]
+        );
+
+        //redirigimos con mensaje de ok
+    return redirect('/adminDestinos')
+        ->with(['mensaje' => 'Destino ' . $destNombre . ' agregado correctamente']);
+    
+});
+
+Route::get('/modificarDestino/{id}', function ($idDestino) {
+
+    $destino = DB::table('destinos') 
+            ->where('idDestino', $idDestino)
+            ->first(); 
+        
+    return view('modificarDestino', [ 'destino' => $destino]); 
+});
